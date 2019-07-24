27a723a8ff319ab28c865ea29002cbb904955adc
EstefanHu <estefanhu074@gmail.com>
2019-07-23 22:49:31 -0700

    init commit

---
+++ src/main/java/edu/uw/info360/Info360projectApplication.java
@@ -0,0 +1,13 @@
+package edu.uw.info360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Info360projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Info360projectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uw/info360/Info360projectApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.uw.info360;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Info360projectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
