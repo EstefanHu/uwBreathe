2b254f8a2e8c0d03f6b507cdb5adb2a0040b86ff
EstefanHu <estefanhu074@gmail.com>
2019-07-25 22:39:11 -0700

    installed tomcat and jstl dependencies

--- src/main/java/edu/uw/info360/repositories/UserRepository.java
+++ src/main/java/edu/uw/info360/repositories/UserRepository.java
@@ -1,5 +1,11 @@
 package edu.uw.info360.repositories;
 
-public interface UserRepository {
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+
+import edu.uw.info360.models.User;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+	List<User> findAll();
 }
--- src/main/java/edu/uw/info360/services/UserService.java
+++ src/main/java/edu/uw/info360/services/UserService.java
@@ -1,5 +1,21 @@
 package edu.uw.info360.services;
 
-public class UserService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import edu.uw.info360.models.User;
+import edu.uw.info360.repositories.UserRepository;
+
+@Service
+public class UserService {
+	private final UserRepository userRepo;
+	
+	public UserService(UserRepository userRepo) {
+		this.userRepo = userRepo;
+	}
+	
+	public List<User> allUsers() {
+		return userRepo.findAll();
+	}
 }
