820990b0977960f7fc341eae8e8e8edbaf344067
EstefanHu <estefanhu074@gmail.com>
2019-07-27 20:15:43 -0700

    installed dependent repositories

---
+++ src/main/java/edu/uw/info360/repositories/PathRepository.java
@@ -0,0 +1,13 @@
+package edu.uw.info360.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.uw.info360.models.Path;
+
+@Repository
+public interface PathRepository extends CrudRepository<Path, Long> {
+	List<Path> findAll();
+}
