481d7b8759025a1b58980ab2e9f0114c82efa90f
EstefanHu <estefanhu074@gmail.com>
2019-07-30 21:34:32 -0700

    Installed test validator for Path

--- src/main/java/edu/uw/info360/validators/PathValidator.java
+++ src/main/java/edu/uw/info360/validators/PathValidator.java
@@ -1,5 +1,27 @@
 package edu.uw.info360.validators;
 
-public class PathValidator {
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import edu.uw.info360.models.Path;
+
+@Component
+public class PathValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Path.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		Path path = (Path) target;
+		
+		if(!path.getId().equals(7)) {
+			errors.rejectValue("title", "This Path is too lucky");
+		}
+		
+	}
 
 }
