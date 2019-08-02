692955494f172c2dc6a99a099280fc632b5090fc
EstefanHu <estefanhu074@gmail.com>
2019-08-01 20:23:42 -0700

    Initialized

---
+++ src/main/java/edu/uw/info360/repositories/PathsNodesRepository.java
@@ -0,0 +1,13 @@
+package edu.uw.info360.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.uw.info360.models.PathsNodes;
+
+@Repository
+public interface PathsNodesRepository extends CrudRepository<PathsNodes, Long> {
+	List<PathsNodes> findAll();
+}
