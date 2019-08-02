01770e729deea7527cb0b2d709f241069e980b0c
EstefanHu <estefanhu074@gmail.com>
2019-08-02 10:49:22 -0700

    Installed registerUser logic

--- src/main/java/edu/uw/info360/controllers/LoginController.java
+++ src/main/java/edu/uw/info360/controllers/LoginController.java
@@ -1,11 +1,29 @@
 package edu.uw.info360.controllers;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import edu.uw.info360.models.User;
+import edu.uw.info360.services.UserService;
+import edu.uw.info360.validators.UserValidator;
 
 @Controller
 @RequestMapping("login")
 public class LoginController {
+	private final UserService userService;
+	private final UserValidator userValidator;
+	
+	public LoginController(UserService userService, UserValidator userValidator) {
+		this.userService = userService;
+		this.userValidator = userValidator;
+	}
+	
 	@RequestMapping("")
 	public String login() {
 		return "Login/login.jsp";
@@ -22,11 +40,18 @@ public class LoginController {
 	
 	@RequestMapping("/register")
 	public String register() {
-		return "Login/register";
+		return "Login/register.jsp";
 	}
 	
-//	
-//	TODO: Create Route to Register User
-//	
+    @RequestMapping(value="/registerUser", method=RequestMethod.POST)
+    public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session) {
+        userValidator.validate(user, result);
+        if(result.hasErrors()) {
+            return "Login/register.jsp";
+        }
+        User u = userService.registerUser(user);
+        session.setAttribute("userId", u.getId());
+        return "redirect:/home";
+    }
 	
 }
