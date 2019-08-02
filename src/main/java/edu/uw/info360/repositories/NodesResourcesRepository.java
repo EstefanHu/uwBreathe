b3072c25b16ab5ca1060f8d56ffb049639d27d2b
EstefanHu <estefanhu074@gmail.com>
2019-08-02 09:21:57 -0700

    Initialized

---
+++ src/main/java/edu/uw/info360/repositories/NodesResourcesRepository.java
@@ -0,0 +1,16 @@
+package edu.uw.info360.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.uw.info360.models.Node;
+import edu.uw.info360.models.NodesResources;
+
+@Repository
+public interface NodesResourcesRepository extends CrudRepository<NodesResources, Long> {
+	List<NodesResources> findAll();
+	
+	List<NodesResources> findByNodeBuUpdatedAtAsc(Node node);
+}
