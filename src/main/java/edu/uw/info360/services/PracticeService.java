e2dbf71b88dac28351c6c5f9b58eba2b1c42d3c3
EstefanHu <estefanhu074@gmail.com>
2019-08-06 19:32:40 -0700

    Installed

---
+++ src/main/java/edu/uw/info360/services/PracticeService.java
@@ -0,0 +1,21 @@
+package edu.uw.info360.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import edu.uw.info360.models.Practice;
+import edu.uw.info360.repositories.PracticeRepository;
+
+@Service
+public class PracticeService {
+	private final PracticeRepository practiceRepo;
+	
+	public PracticeService(PracticeRepository practiceRepo) {
+		this.practiceRepo = practiceRepo;
+	}
+	
+	public List<Practice> findAllPractices() {
+		return practiceRepo.findAll();
+	}
+}
