7a7024d8dda6ab4ba092a30f34bc6fb74f51aa20
EstefanHu <estefanhu074@gmail.com>
2019-08-09 10:54:36 -0700

    Installed theme route

--- src/main/java/edu/uw/info360/controllers/ApiController.java
+++ src/main/java/edu/uw/info360/controllers/ApiController.java
@@ -1,23 +1,31 @@
 package edu.uw.info360.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import edu.uw.info360.models.Resource;
-import edu.uw.info360.services.ResourceService;
+import edu.uw.info360.models.Node;
+import edu.uw.info360.services.NodeService;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-	private final ResourceService resourceService;
+	private final NodeService nodeService;
 	
-	public ApiController(ResourceService resourceService) {
-		this.resourceService = resourceService;
+	public ApiController(NodeService nodeService) {
+		this.nodeService = nodeService;
 	}
 	
 //	TODO Home "Themes" no database call
 	
-//	TODO Theme - call nodes by specific Theme
+	@GetMapping("/{theme}")
+	public List<Node> theme(@PathVariable("theme") String theme) {
+		List<Node> nodes = nodeService.findByTheme(theme);
+		return nodes;
+	}
 	
 //	TODO Location - call node and related practices and resources
 	
