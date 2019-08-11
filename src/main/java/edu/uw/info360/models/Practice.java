62cab06187167a2106d9b7de99d4f6eed1a7a8b2
EstefanHu <estefanhu074@gmail.com>
2019-08-10 22:32:55 -0700

    removed dependencies

--- src/main/java/edu/uw/info360/models/Practice.java
+++ src/main/java/edu/uw/info360/models/Practice.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -14,7 +13,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
