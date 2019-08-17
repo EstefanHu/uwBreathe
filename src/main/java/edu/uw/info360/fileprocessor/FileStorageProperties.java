7bc4becad2b49e686dd7bc81a8c1d563444f790d
EstefanHu <estefanhu074@gmail.com>
2019-08-17 15:54:51 -0700

    Installed image processing logic

---
+++ src/main/java/edu/uw/info360/fileprocessor/FileStorageProperties.java
@@ -0,0 +1,16 @@
+package edu.uw.info360.fileprocessor;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "file")
+public class FileStorageProperties {
+    private String uploadDir;
+
+    public String getUploadDir() {
+        return uploadDir;
+    }
+
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
+}
