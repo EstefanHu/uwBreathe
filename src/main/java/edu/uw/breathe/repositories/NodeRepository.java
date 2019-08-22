c8aac9d99029bcf1bf95c629c2b0679ba1c45e67
EstefanHu <estefanhu074@gmail.com>
2019-08-22 14:12:37 -0700

    Removed unused code

--- src/main/java/edu/uw/breathe/repositories/NodeRepository.java
+++ src/main/java/edu/uw/breathe/repositories/NodeRepository.java
@@ -9,7 +9,6 @@ import edu.uw.breathe.models.Node;;
 
 @Repository
 public interface NodeRepository extends CrudRepository<Node, Long> {
-	List<Node> findAll();
 	List<Node> findAllByOrderByThemeAsc();
 	
 	Node findByTitle(String title);
