063bd276885d90c81da4fa270062d2fd8a154493
EstefanHu <estefanhu074@gmail.com>
2019-08-22 14:12:12 -0700

    Installed

---
+++ src/main/java/edu/uw/breathe/services/CommentService.java
@@ -0,0 +1,19 @@
+package edu.uw.breathe.services;
+
+import org.springframework.stereotype.Service;
+
+import edu.uw.breathe.models.Comment;
+import edu.uw.breathe.repositories.CommentRepository;
+
+@Service
+public class CommentService {
+	private final CommentRepository commentRepo;
+	
+	public CommentService(CommentRepository commentRepo) {
+		this.commentRepo = commentRepo;
+	}
+	
+	public Comment createComment(Comment comment) {
+		return commentRepo.save(comment);
+	}
+}
